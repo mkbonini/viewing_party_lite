dad7d36dacf74e4d37f61b567071e5e61c411333
mkbonini <mkbonini@gmail.com>
2022-09-06 21:06:15 -0600

    test & feat: invalid email and password check

--- spec/features/users/login_spec.rb
+++ spec/features/users/login_spec.rb
@@ -23,4 +23,32 @@ RSpec.describe 'login page' do
 
     expect(current_path).to eq("/users/#{user.id}")
   end
+
+  it 'returns to login with invalid password' do
+    user = User.create!(name: 'ODB', email: 'testemail@mail.com', password: '123test', password_confirmation: '123test')
+
+    visit '/login'
+
+    fill_in('email', with: 'testemail@mail.com')
+    fill_in('password', with: 'test')
+
+    click_on('Log In')
+
+    expect(current_path).to eq("/login")
+    expect(page).to have_content("Invalid Username or Password")
+  end
+
+  it 'returns to login with invalid email' do
+    user = User.create!(name: 'ODB', email: 'testemail@mail.com', password: '123test', password_confirmation: '123test')
+
+    visit '/login'
+
+    fill_in('email', with: 'email@mail.com')
+    fill_in('password', with: '123test')
+
+    click_on('Log In')
+
+    expect(current_path).to eq("/login")
+    expect(page).to have_content("Invalid Username or Password")
+  end
 end
